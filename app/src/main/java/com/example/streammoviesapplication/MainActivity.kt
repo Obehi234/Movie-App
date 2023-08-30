890ee77b0e077bc87b1169f44e1057f8a8be522b
Obehi234 <obehi.ataga234@gmail.com>
2023-08-30 15:06:42 +0100

    Bottom nav icon setup completed

--- app/src/main/java/com/example/streammoviesapplication/MainActivity.kt
+++ app/src/main/java/com/example/streammoviesapplication/MainActivity.kt
@@ -2,14 +2,15 @@ package com.example.streammoviesapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.itemIconTintList = null
     }
-
-
-
 }
