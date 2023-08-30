13f4973d295f7a13eb66a281a1e3f126fa59b3e3
Obehi234 <48982222+Obehi234@users.noreply.github.com>
2023-08-30 16:20:27 +0100

    Bottom nav setup (#2)
    * Created branch for bottom navigation feature
    
    * Removed boilercode textview
    
    * Created menu for nav bar
    
    * Imported Home Icon from design file
    
    * Imported play icon from design file
    
    * Bottom nav set up
    
    * Used styles and theming for more abstraction
    
    * Imported new play icon
    
    * Implemented two icons to be displayed per selected state
    
    * Bottom nav icon setup completed
    
    * Removed nav_color file
    
    * Renamed drawables and removed redundant strings
    
    * Replace png file with svg format for profile icon

--- app/src/main/java/com/example/streammoviesapplication/MainActivity.kt
+++ app/src/main/java/com/example/streammoviesapplication/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.streammoviesapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.itemIconTintList = null
     }
 }
