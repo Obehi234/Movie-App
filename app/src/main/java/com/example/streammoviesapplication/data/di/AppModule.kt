589e2c6f270ea9b682b6c128164e28b8b5ab8f0f
Obehi234 <obehi.ataga234@gmail.com>
2023-08-31 07:45:12 +0100

    Created retrofitService dependency in App Module

--- app/src/main/java/com/example/streammoviesapplication/data/di/AppModule.kt
+++ app/src/main/java/com/example/streammoviesapplication/data/di/AppModule.kt
@@ -1,11 +1,21 @@
 package com.example.streammoviesapplication.data.di
 
+import com.example.streammoviesapplication.network.MovieService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun getRetrofitServiceInstance(retrofit: Retrofit) : MovieService {
+        return retrofit.create(MovieService::class.java)
+    }
 
 }
