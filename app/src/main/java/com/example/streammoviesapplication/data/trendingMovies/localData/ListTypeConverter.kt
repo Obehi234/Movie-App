3317b0aa5441d6aa57ec714227d67f51cd1af9a5
Obehi234 <obehi.ataga234@gmail.com>
2023-08-31 22:47:10 +0100

    Created type converter class to convert data parameter-list of int to string for proper insertion in database

---
+++ app/src/main/java/com/example/streammoviesapplication/data/trendingMovies/localData/ListTypeConverter.kt
@@ -0,0 +1,10 @@
+package com.example.streammoviesapplication.data.trendingMovies.localData
+
+import androidx.room.TypeConverter
+
+class ListTypeConverter {
+    @TypeConverter
+    fun convertListToString(list: List<Int>) : String {
+        return list.joinToString { "," }
+    }
+}
