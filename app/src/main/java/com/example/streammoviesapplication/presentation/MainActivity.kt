020aae59d80da67c3ce993282b7c2ecc1510bbfc
Obehi234 <48982222+Obehi234@users.noreply.github.com>
2023-08-31 07:08:06 +0100

    Di setup (#4)
    * Created base application class to instantiate hilt
    
    * Added base application class to manifest
    
    * Added hilt annotations to Main Activity
    
    * Created AppModule object to define dependencies

--- app/src/main/java/com/example/streammoviesapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/streammoviesapplication/presentation/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.streammoviesapplication.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
