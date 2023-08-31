56e5892b20604e11b1e6b73289ea091ca35c2eeb
Obehi234 <obehi.ataga234@gmail.com>
2023-08-31 12:17:02 +0100

    Created funtion to extract local trending movies data from database

--- app/src/main/java/com/example/streammoviesapplication/data/db/MovieDao.kt
+++ app/src/main/java/com/example/streammoviesapplication/data/db/MovieDao.kt
@@ -3,13 +3,18 @@ package com.example.streammoviesapplication.data.db
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.streammoviesapplication.data.trendingMovies.localData.TrendingMoviesEntity
+import com.example.streammoviesapplication.utils.Constants.MOVIE_DATABASE
 
 @Dao
 interface MovieDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTrendingMoviesToDatabase(movies: List<TrendingMoviesEntity>)
+
+    @Query("SELECT * FROM $MOVIE_DATABASE")
+    suspend fun getAllTrendingMovies() : List<TrendingMoviesEntity>
 
 
 }
