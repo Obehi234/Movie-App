db172663071afdc5f036e3ce5a54b8f0ca5d9d66
Obehi234 <obehi.ataga234@gmail.com>
2023-08-31 07:26:10 +0100

    Created movie service interface to make network call

---
+++ app/src/main/java/com/example/streammoviesapplication/network/MovieService.kt
@@ -0,0 +1,17 @@
+package com.example.streammoviesapplication.network
+
+import com.example.streammoviesapplication.data.trendingMovies.TrendingMoviesResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+
+interface MovieService {
+
+    @GET("/3/trending/movie/day")
+    suspend fun getAllTrendingMovies(
+        @Header("Authorization") authorizationHeader: String,
+        @Query("language")  language: String
+    ) : Response<TrendingMoviesResponse>
+}
