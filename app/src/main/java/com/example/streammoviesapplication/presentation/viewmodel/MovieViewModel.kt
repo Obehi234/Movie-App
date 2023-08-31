108c8d007d6f897e98eab58a391c449953ef06d2
Obehi234 <obehi.ataga234@gmail.com>
2023-08-31 21:57:05 +0100

    Added observable parameter trendingMovieList to observe the flow

--- app/src/main/java/com/example/streammoviesapplication/presentation/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/example/streammoviesapplication/presentation/viewmodel/MovieViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.streammoviesapplication.presentation.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.streammoviesapplication.data.repository.TrendingMoviesRepository
 import com.example.streammoviesapplication.data.trendingMovies.localData.TrendingMoviesEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,10 +17,15 @@ import javax.inject.Inject
 class MovieViewModel @Inject constructor(
     private val repository: TrendingMoviesRepository
 ): ViewModel() {
+    private val _trendingMovieList =  MutableStateFlow<List<TrendingMoviesEntity>>(emptyList())
+    val trendingMovieList = _trendingMovieList.asStateFlow()
 
 init {
     viewModelScope.launch{
-        val trendingMovieList = repository.fetchTrendingMovies()
+         repository.fetchTrendingMovies().collect{trendingMovies ->
+             _trendingMovieList.value = trendingMovies
+             Log.d("CHECK FLOW", "Trending Movie List - $trendingMovies")
+         }
     }
 }
 
