5ca9091219808d286d2b2bd2b9eca02d1e97d3ab
Obehi234 <obehi.ataga234@gmail.com>
2023-08-31 21:14:49 +0100

    Implemented my flow variable in my viewmodel

--- app/src/main/java/com/example/streammoviesapplication/presentation/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/example/streammoviesapplication/presentation/viewmodel/MovieViewModel.kt
@@ -1,14 +1,24 @@
 package com.example.streammoviesapplication.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.streammoviesapplication.data.repository.TrendingMoviesRepository
+import com.example.streammoviesapplication.data.trendingMovies.localData.TrendingMoviesEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MovieViewModel @Inject constructor(
-    private val trendingMoviesRepository: TrendingMoviesRepository
+    private val repository: TrendingMoviesRepository
 ): ViewModel() {
+
+init {
+    viewModelScope.launch{
+        val trendingMovieList = repository.fetchTrendingMovies()
+    }
+}
 
 
 
