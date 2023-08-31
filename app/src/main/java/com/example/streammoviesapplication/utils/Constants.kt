57cfe00c1854c36f9f2707c06834d056ba5d94e3
Obehi234 <obehi.ataga234@gmail.com>
2023-08-31 07:35:50 +0100

    Added authorization header to constants

--- app/src/main/java/com/example/streammoviesapplication/utils/Constants.kt
+++ app/src/main/java/com/example/streammoviesapplication/utils/Constants.kt
@@ -4,4 +4,5 @@ import com.example.streammoviesapplication.BuildConfig
 
 object Constants {
     const val API_KEY = BuildConfig.API_KEY
+    const val AuthorizationHeader = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTM5NzVlMmVjNWQ2ODBmN2UxMmRjMjcwMTg5MTNmZiIsInN1YiI6IjY0OWMwYzIzYWY1OGNiMDEzOTY5YjJmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u_Pv46tm1GHBCoPZ1jrbq2D5bNoDz55AtOHiEggVt3E"
 }
