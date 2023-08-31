07631e13fa1905c2349f231b8000b2bf7ce70ee2
Obehi234 <obehi.ataga234@gmail.com>
2023-08-31 12:12:35 +0100

    Created function to insert remote data to database

--- app/src/main/java/com/example/streammoviesapplication/data/db/MovieDao.kt
+++ app/src/main/java/com/example/streammoviesapplication/data/db/MovieDao.kt
@@ -1,7 +1,15 @@
 package com.example.streammoviesapplication.data.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.example.streammoviesapplication.data.trendingMovies.localData.TrendingMoviesEntity
 
 @Dao
-object MovieDao {
+interface MovieDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTrendingMoviesToDatabase(movies: List<TrendingMoviesEntity>)
+
+
 }
