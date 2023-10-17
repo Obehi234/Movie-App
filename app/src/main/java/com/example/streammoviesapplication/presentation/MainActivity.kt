c21058adbb83b4043f9ee39b95828fff72258a08
obehi234 <obehi.ataga234@gmail.com>
2023-10-17 05:49:25 +0100

    Creating screen 2 UI underway

--- app/src/main/java/com/example/streammoviesapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/streammoviesapplication/presentation/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.streammoviesapplication.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.streammoviesapplication.R
 import com.example.streammoviesapplication.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,12 +18,17 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         val bottomNavigationView = binding.bottomNav
         bottomNavigationView.itemIconTintList = null
+
+    }
+
+        override fun onSupportNavigateUp(): Boolean {
+            val navController = findNavController(R.id.fragmentContainerView)
+            return navController.navigateUp() || super.onSupportNavigateUp()
+        }
     }
 
 
 
 
-}
