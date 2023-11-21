86e9b99d8d616efcf0e172ff4b72917b859846a7
obehi234 <obehi.ataga234@gmail.com>
2023-11-21 12:44:15 +0100

    Created recyclerview for movieTab

--- app/src/main/java/com/example/streammoviesapplication/presentation/tabViews/MoviesFragment.kt
+++ app/src/main/java/com/example/streammoviesapplication/presentation/tabViews/MoviesFragment.kt
@@ -2,23 +2,17 @@ package com.example.streammoviesapplication.presentation.tabViews
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import com.example.streammoviesapplication.R
 import com.example.streammoviesapplication.data.model.localData.result
 import com.example.streammoviesapplication.databinding.FragmentMoviesBinding
 import com.example.streammoviesapplication.presentation.adapter.MovieListAdapter
 import com.example.streammoviesapplication.presentation.viewmodel.tabViewModel.TabViewModel
 import com.example.streammoviesapplication.utils.resource.Resource
-import kotlinx.coroutines.launch
 
 class MoviesFragment : Fragment() {
     private var _binding: FragmentMoviesBinding? = null
