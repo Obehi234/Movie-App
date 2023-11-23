7d4c46b79dad525f53a41dcc40fc423a5458fbca
obehi234 <obehi.ataga234@gmail.com>
2023-11-23 05:02:14 +0100

    Abstracted image url in MovieViewPagerAdapter

--- app/src/main/java/com/example/streammoviesapplication/presentation/adapter/MovieViewPagerAdapter.kt
+++ app/src/main/java/com/example/streammoviesapplication/presentation/adapter/MovieViewPagerAdapter.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.streammoviesapplication.data.model.localData.TrendingMoviesEntity
 import com.example.streammoviesapplication.databinding.HomeNavMoviePosterItemBinding
+import com.example.streammoviesapplication.utils.Constants.BASE_IMAGE_URL
 
 
 class MovieViewPagerAdapter(private val imageList: List<TrendingMoviesEntity>) : RecyclerView.Adapter<MovieViewPagerAdapter.ViewPagerHolder>(){
@@ -13,7 +14,7 @@ class MovieViewPagerAdapter(private val imageList: List<TrendingMoviesEntity>) :
     inner class ViewPagerHolder(val binding: HomeNavMoviePosterItemBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: TrendingMoviesEntity) {
             binding.apply{
-                ivPoster.load("https://image.tmdb.org/t/p/w500" + item.poster_path)
+                ivPoster.load(BASE_IMAGE_URL + item.poster_path)
                 tvRatingNumber.text = item.vote_average.toString()
                 tvMovieName.text = item.title
             }
