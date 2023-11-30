96b01f813415e6632ace450b2a54849efe70a9a6
obehi234 <obehi.ataga234@gmail.com>
2023-11-30 06:56:00 +0100

    Set up skeleton for details page horizontal adapter

---
+++ app/src/main/java/com/example/streammoviesapplication/presentation/adapter/detailsHorizontalAdapter/DetailsHorizontalAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.streammoviesapplication.presentation.adapter.detailsHorizontalAdapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.streammoviesapplication.databinding.ItemMovieDetailsBinding
+
+class DetailsHorizontalAdapter: ListAdapter<Result, DetailsHorizontalAdapter.DetailsViewHolder>(DetailsDiffUtilCallback()) {
+     inner class DetailsViewHolder (private val binding:ItemMovieDetailsBinding) : RecyclerView.ViewHolder(binding.root){
+
+
+    }
+
+    class DetailsDiffUtilCallback: DiffUtil.ItemCallback<Result>(){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: DetailsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+}
+
+
