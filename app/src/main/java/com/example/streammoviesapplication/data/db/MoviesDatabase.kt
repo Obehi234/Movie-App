d78c908f3b3aab9f0c089641ed5248dacc1e3040
obehi234 <obehi.ataga234@gmail.com>
2023-12-02 06:26:59 +0100

    Added movie details entity to database

--- app/src/main/java/com/example/streammoviesapplication/data/db/MoviesDatabase.kt
+++ app/src/main/java/com/example/streammoviesapplication/data/db/MoviesDatabase.kt
@@ -3,15 +3,20 @@ package com.example.streammoviesapplication.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.streammoviesapplication.data.model.localData.MovieDetailsEntity
 import com.example.streammoviesapplication.data.model.localData.MovieResultEntity
 import com.example.streammoviesapplication.data.model.localData.TrendingMoviesEntity
 
-@Database(entities = [
-    TrendingMoviesEntity::class,
-    MovieResultEntity::class
-                     ], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        TrendingMoviesEntity::class,
+        MovieResultEntity::class,
+        MovieDetailsEntity::class
+    ], version = 1, exportSchema = false
+)
 @TypeConverters(ListTypeConverter::class)
 abstract class MoviesDatabase : RoomDatabase() {
-    abstract fun trendingMoviesDao() : TrendingMoviesDao
-    abstract fun movieListDao() : MovieListDao
+    abstract fun trendingMoviesDao(): TrendingMoviesDao
+    abstract fun movieListDao(): MovieListDao
+    abstract fun movieDetailsDao() : MovieDetailsDao
 }
