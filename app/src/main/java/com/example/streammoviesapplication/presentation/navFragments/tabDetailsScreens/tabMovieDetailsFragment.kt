7cd56204a37d8a5183ac077bcc5632f10308b8a3
obehi234 <obehi.ataga234@gmail.com>
2023-12-08 15:41:26 +0100

    Set up back navigation with back icon

--- app/src/main/java/com/example/streammoviesapplication/presentation/navFragments/tabDetailsScreens/tabMovieDetailsFragment.kt
+++ app/src/main/java/com/example/streammoviesapplication/presentation/navFragments/tabDetailsScreens/tabMovieDetailsFragment.kt
@@ -5,17 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.streammoviesapplication.R
+import com.example.streammoviesapplication.databinding.FragmentTabMovieDetailsBinding
+import com.example.streammoviesapplication.presentation.navFragments.HomeFragmentDirections
 
 
 class tabMovieDetailsFragment : Fragment() {
+    private var _binding: FragmentTabMovieDetailsBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_tab_movie_details, container, false)
+        _binding = FragmentTabMovieDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val icBackNav = binding.backIconNav
+        icBackNav.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
 
 
 }
