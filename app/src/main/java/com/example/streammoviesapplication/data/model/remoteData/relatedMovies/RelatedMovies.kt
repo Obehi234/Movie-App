6f9854a9af3b54db32e6181c08d8f9d6c7140f6f
obehi234 <obehi.ataga234@gmail.com>
2023-12-08 22:09:20 +0100

    Extracted data for related movies from end point

---
+++ app/src/main/java/com/example/streammoviesapplication/data/model/remoteData/relatedMovies/RelatedMovies.kt
@@ -0,0 +1,8 @@
+package com.example.streammoviesapplication.data.model.remoteData.relatedMovies
+
+data class RelatedMovies(
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
