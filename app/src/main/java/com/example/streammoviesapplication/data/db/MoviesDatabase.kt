1b41c5d1ad47a50969dc1cae92426bef668ffd00
obehi234 <obehi.ataga234@gmail.com>
2023-12-08 22:17:59 +0100

    Added related entity table to database

--- app/src/main/java/com/example/streammoviesapplication/data/db/MoviesDatabase.kt
+++ app/src/main/java/com/example/streammoviesapplication/data/db/MoviesDatabase.kt
@@ -5,13 +5,15 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.streammoviesapplication.data.model.localData.MovieDetailsEntity
 import com.example.streammoviesapplication.data.model.localData.MovieResultEntity
+import com.example.streammoviesapplication.data.model.localData.RelatedMoviesEntity
 import com.example.streammoviesapplication.data.model.localData.TrendingMoviesEntity
 
 @Database(
     entities = [
         TrendingMoviesEntity::class,
         MovieResultEntity::class,
-        MovieDetailsEntity::class
+        MovieDetailsEntity::class,
+        RelatedMoviesEntity::class
     ], version = 1, exportSchema = false
 )
 @TypeConverters(ListTypeConverter::class, GenreTypeConverter::class)
---
+++ app/src/main/java/com/example/streammoviesapplication/data/db/RelatedMoviesDao.kt
@@ -0,0 +1,4 @@
+package com.example.streammoviesapplication.data.db
+
+interface RelatedMoviesDao {
+}
