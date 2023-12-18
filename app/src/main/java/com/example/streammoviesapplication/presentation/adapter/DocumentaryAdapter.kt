c767306f9a72e9ab56f4777883f053095c660ebd
obehi234 <obehi.ataga234@gmail.com>
2023-12-18 10:40:23 +0100

    Prepared adapter for documentary tab

---
+++ app/src/main/java/com/example/streammoviesapplication/presentation/adapter/DocumentaryAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.streammoviesapplication.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.streammoviesapplication.data.model.localData.DocumentaryEntity
+import com.example.streammoviesapplication.databinding.MovieTabRecyclerItemBinding
+import com.example.streammoviesapplication.utils.Constants
+
+class DocumentaryAdapter : ListAdapter<DocumentaryEntity, DocumentaryAdapter.DocumentaryViewHolder>(
+    DocumentaryDiffUtilCallback()
+) {
+
+    inner class DocumentaryViewHolder(private val binding: MovieTabRecyclerItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: DocumentaryEntity) {
+            binding.apply {
+                movieCardBg.load(Constants.BASE_IMAGE_URL + item.poster_path)
+                movieTv.text = item.original_title
+            }
+        }
+    }
+
+    class DocumentaryDiffUtilCallback : DiffUtil.ItemCallback<DocumentaryEntity>() {
+        override fun areItemsTheSame(
+            oldItem: DocumentaryEntity,
+            newItem: DocumentaryEntity
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: DocumentaryEntity,
+            newItem: DocumentaryEntity
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DocumentaryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = MovieTabRecyclerItemBinding.inflate(inflater, parent, false)
+        return DocumentaryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DocumentaryViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
